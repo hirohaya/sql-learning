81808caf8bf0cbc74bc98ea69c22c176e22224da
Lucas Hiroshi Hayashida <hayashida@assembly.linux.ime.usp.br>
2016-06-06 09:56:25 -0300

    First 4 lessons, even though there are 3 files.

---
+++ ex3.sql
@@ -0,0 +1,13 @@
+INSERT INTO person(id, first_name, last_name, age)
+	VALUES(0, "Zed", "Shaw", 37);
+
+INSERT INTO person VALUES(1, "Lucas", "Hayashida", 25);
+
+INSERT INTO pet(id, name, breed, age, dead)
+	VALUES(0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet	VALUES(2, "John Cena", "Cat", 0.9, 0); 
+
+
+INSERT INTO pet VALUES(1, "Gigantor", "Robot", 1, 1);
+
